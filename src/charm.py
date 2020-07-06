12c41d6d22a4195546b32cd1be5d3e77e7ed0d0b
jamesbeedy <jamesbeedy@gmail.com>
2020-07-06 14:44:52 -0700

    dev

--- src/charm.py
+++ src/charm.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env python3
-from ops.charm import CharmBase
-
-from ops.main import main
-
-
+import collections
 import logging
 import socket
 import json
+
+
+from ops.charm import CharmBase
+
+from ops.main import main
 
 from ops.framework import (
     Object,
@@ -16,6 +17,23 @@ from ops.framework import (
 
 
 logger = logging.getLogger()
+
+
+def dict_keys_without_hyphens(a_dict):
+    """Return the a new dict with underscores instead of hyphens in keys.
+    https://github.com/juju/charm-helpers/blob/f9c06a96b0d1587a1c94d4d398efde8a403026eb/charmhelpers/contrib/templating/contexts.py#L31,L34
+    """
+    return dict(
+        (key.replace('-', '_'), val) for key, val in a_dict.items())
+
+
+def get_partitions(node_data):
+    part_dict = collections.defaultdict(dict)
+    for node in node_data:
+        part_dict[node['partition']].setdefault('hosts', [])
+        part_dict[node['partition']]['hosts'].append(node['hostname'])
+        part_dict[node['partition']]['default'] = node['default']
+    return dict(part_dict)
 
 
 class RequirerRelationEvents(ObjectEvents):
@@ -57,9 +75,44 @@ class TestingRequirerRelation(Object):
             self._on_relation_broken
         )
 
+    def get_partitions(self):
+        return self._partitions
+
+    def get_node_data(self):
+        return self._nodes
+
     @property
     def _relation(self):
         return self.framework.model.get_relation(self._relation_name)
+
+    @property
+    def _partitions(self):
+        return get_partitions(self._get_all_nodes)
+
+    @property
+    def _nodes(self):
+        """
+        node_info = []
+        for relation in relations:
+            for unit in relation.units:
+                node_info.append(relation.data[unit]['node_info'])
+        return node_info
+        """
+        node_info_keys = {
+            'ingress-address', 'hostname',
+            'partition', 'inventory', 'default',
+        }
+
+        return [
+            [
+                dict_keys_without_hyphens({
+                    k: relation.data[unit][k]
+                    for k in node_info_keys
+                })
+                for unit in relation.units
+            ]
+            for relation in self.framework.model.relations['slurmd']
+        ]
 
     def _on_relation_created(self, event):
         logger.debug("################ LOGGING RELATION CREATED ####################")
@@ -70,6 +123,7 @@ class TestingRequirerRelation(Object):
 
     def _on_relation_joined(self, event):
         logger.debug("################ LOGGING RELATION JOINED ####################")
+
         #logger.debug(self._relation)
         #logger.debug(event.relation.data)
         #logger.debug("################ LOGGING EVENT DATA in RELATION JOINED ####################")
@@ -77,12 +131,18 @@ class TestingRequirerRelation(Object):
         #logger.debug(event.relation.data[self.model.unit])
         #logger.debug("################ LOGGING SELF RELATION DATA in RELATION JOINED ####################")
         #logger.debug(self._relation)
-        #logger.debug(self._relation.data)
+        #loggevent.relation.data.geter.debug(self._relation.data)
 
     def _on_relation_changed(self, event):
         logger.debug("################ LOGGING RELATION CHANGED ####################")
-        self.framework.breakpoint('testing-requirer-on-relation-joined')
-        
+        for relation in self.framework.model.relations['slurmd']:
+            for unit in relation.units:
+                for key in ['ingress-address', 'hostname', 'partition', 'inventory', 'default']:
+                    if not relation.data[unit].get(key):
+                        event.defer()
+                        return
+        self.framework.breakpoint("in-relation-changed")
+
     def _on_relation_departed(self, event):
         logger.debug("################ LOGGING RELATION DEPARTED ####################")
 
